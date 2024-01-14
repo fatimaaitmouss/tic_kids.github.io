d3be1fac579bab6e7a7057f2e658acbc95cccb6b
sramos_94 <155068821+sramos94@users.noreply.github.com>
2024-01-14 09:25:32 +0100

    modif

--- js/costum.js
+++ js/costum.js
@@ -1,7 +1,7 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+      x.className += "responsive";
     } else {
       x.className = "topnav";
     }
@@ -30,3 +30,26 @@ function myFunction() {
     document.getElementById(age).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+
+
+  //slider
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
